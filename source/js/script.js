24f45d738e1a3762b5f4a15d839cb20252fd98b4
viktor kan <viktorkan9@gmail.com>
2019-10-20 17:14:13 +0300

    доделал шапку почти

---
+++ source/js/script.js
@@ -0,0 +1,16 @@
+const buttonTrigger = document.querySelector('.menu-controll');
+const header = document.querySelector('.header');
+const menu = document.querySelector('.categories-menu');
+
+const category = menu.querySelector('.categories-menu__link--mobile');
+const categoryMenu = menu.querySelector('.categories-menu__list-inner');
+
+buttonTrigger.addEventListener('click', () => {
+    header.classList.toggle('header--open-menu');
+    menu.classList.toggle('categories-menu--open');
+})
+
+category.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    categoryMenu.classList.toggle('categories-menu__list-inner--open');
+})
